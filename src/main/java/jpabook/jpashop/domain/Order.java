5ea75a0afcb1d0b7e80ab1c47440903cdc5a056b
jssa428428 <jinwoo2328@gmail.com>
2024-05-07 00:21:50 +0900

    domain 작성 완료

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    // JPQL select o From o; -> SQL select * from order n + 1 문제 발생
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    // OneToMany, ManyToOne은 기본적으로 LAZY로 설정되어 있으므로, 따로 설정해줄 필요 X
+    // CascadeType.ALL: Order를 persist하면 OrderItem도 알아서 persist
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate; // 주문시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; // 주문상태로 Order, Cancel로 구분
+
+    // 연관관계 편의 메서드
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+}
